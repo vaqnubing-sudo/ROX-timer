85eb0404f1b70d0f7cac7c755ec70530161059d0
vaqnubing-sudo <vaqnubing@gmail.com>
2025-09-13 18:32:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,52 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
+// sw.js - Service Worker
+
+// Install event
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Installed');
+  self.skipWaiting(); // Activate immediately
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated');
+// Activate event
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activated');
+  return self.clients.claim();
 });
 
-self.addEventListener('notificationclick', event => {
+// Listen for push messages from your main app
+self.addEventListener('push', (event) => {
+  let data = {
+    title: 'Timer',
+    body: 'Timer finished!',
+    icon: 'images/Phreeoni.png'
+  };
+
+  if (event.data) {
+    data = event.data.json();
+  }
+
+  const options = {
+    body: data.body,
+    icon: data.icon,
+    badge: data.icon,
+    vibrate: [200, 100, 200],
+    tag: 'timer-notification'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+// Notification click event
+self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow('https://vaqnubing-sudo.github.io/ROX-timer/')
+    self.clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) {
+        clientList[0].focus();
+      } else {
+        self.clients.openWindow('https://vaqnubing-sudo.github.io/ROX-timer/');
+      }
+    })
   );
 });
